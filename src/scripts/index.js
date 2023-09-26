c6e49be650a4b24b5f863ac4a52051dd26ea12c1
Muhamad Septian Jaelani <mseptianjaelani@gmail.com>
2023-09-26 10:55:01 +0700

    Finish Add-Book Element

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,10 @@
 const searchEl = document.getElementById('search-book-form');
+const addEl = document.getElementById('add-book-form');
 
 searchEl.addEventListener('submit', (event) => {
+	event.preventDefault();
+});
+
+addEl.addEventListener('submit', (event) => {
 	event.preventDefault();
 });
